3eee087fc39a1b7fb1cd795f475e6b77bc3955a0
rlvandaveer@gmail.com <rlvandaveer@gmail.com>
2016-09-09 11:28:16 -0400

    File comment and copyright updates

--- src/Heliar.Composition.Mvc/MvcDependencyRegistrar.cs
+++ src/Heliar.Composition.Mvc/MvcDependencyRegistrar.cs
@@ -1,37 +1,59 @@
-﻿using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
+﻿// ***********************************************************************
+// Assembly         : Heliar.Composition.Mvc
+// Author           : R. L. Vandaveer
+// Created          : 10-23-2015
+//
+// Last Modified By : R. L. Vandaveer
+// Last Modified On : 09-01-2016
+// ***********************************************************************
+// <copyright file="MvcDependencyRegistrar.cs" company="">
+//	Copyright ©2015 - 2016 R. L. Vandaveer. Permission is hereby granted,
+//	free of charge, to any person obtaining a copy of this software and
+//	associated documentation files (the "Software"), to deal in the Software
+//	without restriction, including without limitation the rights to use, copy,
+//	modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//	Software, and to permit persons to whom the Software is furnished to do so,
+//	subject to the following conditions: The above copyright notice and this
+//	permission notice shall be included in all copies or substantial portions
+//	of the Software.
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//	IN THE SOFTWARE.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Registration;
-using System.Reflection;
 using System.Web.Mvc;
 
 using Heliar.Composition.Core;
-using Heliar.Composition.Web;
 
 namespace Heliar.Composition.Mvc
 {
 	/// <summary>
 	/// Class responsible for registering MVC dependencies.
 	/// </summary>
+	/// <seealso cref="Heliar.Composition.Core.ILibraryDependencyRegistrar" />
 	public class MvcDependencyRegistrar : ILibraryDependencyRegistrar
 	{
 		/// <summary>
-		/// Registers the specified catalog.
+		/// Adds the necessary registrations to perform composition for MVC.
 		/// </summary>
-		/// <param name="catalog">The catalog.</param>
-		public void Register(AggregateCatalog catalog)
+		/// <remarks>
+		/// Note that many MVC types like filters are composed by
+		/// custom providers and not the stock resolver and therefore aren't
+		/// configured here.
+		/// </remarks>
+		/// <param name="registrations">The registrations.</param>
+		public void Register(RegistrationBuilder registrations)
 		{
-			var conventions = new RegistrationBuilder();
-			conventions.ForTypesDerivedFrom<IController>()
+			registrations.ForTypesDerivedFrom<IController>()
 				.SetCreationPolicy(CreationPolicy.NonShared)
 				.Export();
-			conventions.ForTypesDerivedFrom<FilterAttribute>()
-				.SetCreationPolicy(CreationPolicy.NonShared)
-				.Export();
-			conventions.ForTypesDerivedFrom<IModelBinder>()
-				.SetCreationPolicy(CreationPolicy.NonShared)
-				.Export();
-			var mvcCatalog = new AssemblyCatalog(HeliarCompositionProvider.WebApplicationAssembly, conventions);
-			catalog.Catalogs.Add(mvcCatalog);
 		}
 	}
 }
--- src/Heliar.Composition.Mvc/MvcDependencyResolutionConfigurator.cs
+++ src/Heliar.Composition.Mvc/MvcDependencyResolutionConfigurator.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : Heliar.Composition.Mvc
+// Author           : R. L. Vandaveer
+// Created          : 10-23-2015
+//
+// Last Modified By : R. L. Vandaveer
+// Last Modified On : 11-12-2015
+// ***********************************************************************
+// <copyright file="MvcDependencyResolutionConfigurator.cs" company="">
+//	Copyright ©2015 - 2016 R. L. Vandaveer. Permission is hereby granted,
+//	free of charge, to any person obtaining a copy of this software and
+//	associated documentation files (the "Software"), to deal in the Software
+//	without restriction, including without limitation the rights to use, copy,
+//	modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//	Software, and to permit persons to whom the Software is furnished to do so,
+//	subject to the following conditions: The above copyright notice and this
+//	permission notice shall be included in all copies or substantial portions
+//	of the Software.
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//	IN THE SOFTWARE.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -9,6 +37,7 @@ namespace Heliar.Composition.Mvc
 	/// <summary>
 	/// Class that configures dependency resolution for MVC.
 	/// </summary>
+	/// <seealso cref="Heliar.Composition.Web.IDependencyResolutionConfigurator" />
 	public class MvcDependencyResolutionConfigurator : IDependencyResolutionConfigurator
 	{
 		/// <summary>
