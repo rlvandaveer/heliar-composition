a772f0dc194dfdd87742d600fe5fd9b3473e08fd
rlvandaveer@gmail.com <rlvandaveer@gmail.com>
2016-09-29 17:29:22 -0400

    Added nuspec. New builds and versions for Nuget deployment.

--- src/Heliar.Composition.Core/Properties/AssemblyInfo.cs
+++ src/Heliar.Composition.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Heliar.Composition.Core")]
-[assembly: AssemblyDescription("Provides core composition services for Heliar Composition")]
+[assembly: AssemblyDescription("Provides a framework for wiring up and bootstrapping MEF based dependencies.")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("33184a48-3be1-4717-bdb1-a1a983da73e0")]
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
-[assembly: AssemblyInformationalVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.16273.2103")]
+[assembly: AssemblyFileVersion("1.0.16273.2103")]
+[assembly: AssemblyInformationalVersion("1.0.16273.2038")]
--- src/Heliar.Composition.Web/Properties/AssemblyInfo.cs
+++ src/Heliar.Composition.Web/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Heliar.Composition.Web")]
-[assembly: AssemblyDescription("Provides core composition services for web based projects")]
+[assembly: AssemblyDescription("Provides shared types and core behavior, e.g. dependency resolver bootstrappers, composition scopes, to perform composition for web based projects")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("39a5ad59-0a65-4032-9b9c-1f0933410b4a")]
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
-[assembly: AssemblyInformationalVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.16273.2103")]
+[assembly: AssemblyFileVersion("1.0.16273.2103")]
+[assembly: AssemblyInformationalVersion("1.0.16273.2038")]
