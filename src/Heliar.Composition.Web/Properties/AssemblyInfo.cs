2d1527eff359b9377ec038667b4544d1436820bc
R. L. Vandaveer <rlvandaveer@gmail.com>
2016-12-14 16:27:16 -0500

    Bump version for release 1.0.16349-alpha2

--- src/Heliar.Composition.Web/Properties/AssemblyInfo.cs
+++ src/Heliar.Composition.Web/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.16349.2022")]
-[assembly: AssemblyFileVersion("1.0.16349.2022")]
-[assembly: AssemblyInformationalVersion("1.0.16349-alpha1")]
+[assembly: AssemblyVersion("1.0.16349.2124")]
+[assembly: AssemblyFileVersion("1.0.16349.2124")]
+[assembly: AssemblyInformationalVersion("1.0.16349-alpha2")]
